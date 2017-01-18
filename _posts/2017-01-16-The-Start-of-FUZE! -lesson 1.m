b4d36b0441fef6e21f42b5240d189860213010fb
theipod3339 <niklas.damarell@challoners.org>
2017-01-18 13:08:09 +0000

    Update 2017-01-16-The-Start-of-FUZE! -lesson 1.m

--- _posts/2017-01-16-The-Start-of-FUZE! -lesson 1.m
+++ _posts/2017-01-16-The-Start-of-FUZE! -lesson 1.m
@@ -10,9 +10,9 @@ published: true
 <body>
 <table>
   <tr>
-    <td>Title</td>
+    <th>Title</th>
     <td>The Start of FUZE - lesson 1</td>
-    <td>Date</td>
+    <th>Date</th>
     <td>16/1/17</td>
   </tr>
 </table>
@@ -20,16 +20,16 @@ published: true
 
 <table>
   <tr>
-    <td>Starting point:</td>
+    <th>Starting point:</th>
     <td>Explanation of FUZE</td>
   </tr>
   <tr>
-    <td>Target for this lesson?</td>
+    <th>Target for this lesson?</th>
     <td>To get to know the system</td>
   </tr>
   <tr>
-    <td>Did I reach my target? 
-(add details to "Lesson Review")</td>
+    <th>Did I reach my target? 
+(add details to "Lesson Review")</th>
     <td> Yes, I played around with the basic system and how to print things, also creating a cool matrix style pattern. I was beginning to look at variables but I ran out of time. </td>
   </tr>
 </table>
@@ -37,22 +37,22 @@ published: true
 
 <table>
   <tr>
-    <td>Lesson Review</td>
+    <th>Lesson Review</th>
   </tr>
   <tr>
-    <td>How did I learn? What strategies were effective? </td>
+    <th>How did I learn? What strategies were effective? </th>
   </tr>
   <tr>
     <td>I learnt the basic functions of FUZE. I learnt how to setup the FUZE setup. I also learnt how to Print things. I printed this matrix running code thing where numbers just rolled across the screen like this: 1 4 6 7 3 5 8 9 6 7 8 3  2 5 6 8 7 7 6 5 5 7  8 9 9 8  6 5 4 3 2 1 2 2 3 4 5  6 7 8 8 9 9 8 7 6 6 7 8 7 7 7 8 77  64 564 8564 58 76 6 5 59 965 65 985 8 596 5 659 596 56 75 3 2 2 34 5 7 8 88 7  66 66 6 6 67  8 99 9 9 9y   0 0 0 99 9  8 8 7 6 5 4 3 2 34 565 456 7654 45678 7654 56788 7654 32 1234 67  6543</td>
   </tr>
   <tr>
-    <td>What limited my learning? Which habits do I need to work on?</td>
+    <th>What limited my learning? Which habits do I need to work on?</th>
   </tr>
   <tr>
     <td>I think helping Felix added and hindered my learning. I suppose I learnt how to teach, but at the same time I slowed down my personal working. Also me and Felix both got distracted by Harvey and Joel who were always talking to us and showing us their new piece of code.</td>
   </tr>
   <tr>
-    <td>What will I change for next time? How will I improve my learning?</td>
+    <th>What will I change for next time? How will I improve my learning?</th>
   </tr>
   <tr>
     <td>I will try get less distracted by others, that way I will learn more. I will also try to read the instructions more before rushing off to do my own thing. </td>
